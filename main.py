dedaaba494826a6acb5062128ad0fca73d6a08a4
Akira <65568815+Akira07210@users.noreply.github.com>
2026-04-02 12:36:22 +0200

    Refactor log_visit to include user agent info
    Updated log_visit function to include user agent parsing and simplified debug output.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # Lib
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
-from user_agents import parse
 # Mes fichier
 from log import log_the_visit_in_CSV
 
@@ -19,17 +18,10 @@ class Data(BaseModel):
 async def log_visit(data: Data, request: Request):
 
     client_ip = request.client.host
-    browser= user_agent.browser.family,
-    browser_version= user_agent.browser.version_string,
-    os= user_agent.os.family,
-    os_version= user_agent.os.version_string,
-    device= user_agent.device.family,
-    is_mobile= user_agent.is_mobile,
-    is_tablet= user_agent.is_tablet,
-    is_pc= user_agent.is_pc
+    user_agent = request.headers.get("user-agent")
     data_track = data.dict()
 
-    print(f"DEBUG : {client_ip},{browser},{os},{device},mobile : {is_mobile}, PC : {is_pc}\n Data track : {}")
+    print(f"DEBUG : {client_ip}, Data track : {data_track}")
     return {
         "ip": client_ip,
         "received": data.dict()  # transforme le Pydantic model en dict
