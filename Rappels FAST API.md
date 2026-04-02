10efe15fa887f6fe8fcfc9b9abe25a2f8a4628eb
Ordi SOLEM <martin.fayolle@gmail.com>
2026-04-02 12:00:07 +0200

    Premier Endpoint pour vérifier la réception de données.

--- Rappels FAST API.md
+++ Rappels FAST API.md
@@ -1,1 +1,43 @@
 **Lancer le serveur :** python3 -m uvicorn main:app --reload
+
+
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+@app.get("/userinfo")
+async def get_user_info(request: Request):
+    user_agent = request.headers.get("user-agent")
+    client_host = request.client.host
+    client_port = request.client.port
+
+    return {
+        "user_agent": user_agent,
+        "ip": client_host,
+        "port": client_port
+    }
+from fastapi import FastAPI, Request
+from user_agents import parse
+
+app = FastAPI()
+
+
+
+
+
+
+@app.get("/userinfo")
+async def get_user_info(request: Request):
+    ua_string = request.headers.get("user-agent")
+    user_agent = parse(ua_string)
+
+    return {
+        "browser": user_agent.browser.family,
+        "browser_version": user_agent.browser.version_string,
+        "os": user_agent.os.family,
+        "os_version": user_agent.os.version_string,
+        "device": user_agent.device.family,
+        "is_mobile": user_agent.is_mobile,
+        "is_tablet": user_agent.is_tablet,
+        "is_pc": user_agent.is_pc
+    }
--- main.py
+++ main.py
@@ -1,25 +1,35 @@
+# Lib
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
+from user_agents import parse
+# Mes fichier
+from log import log_the_visit_in_CSV
 
 app = FastAPI()
-
-
     
 ###################################################
 ################## MES ENDPOINTS ##################
 
 # Définition du modèle de données attendu
 class Data(BaseModel):
-    test: int
-    # referrer: str
-    # screen: int
-    # id: int
-    # utcoffset: str
+    page: str
+    screen : str
 
 @app.get("/log_your_visit")
 async def log_visit(data: Data, request: Request):
+
     client_ip = request.client.host
-    # Fusion des données avec l'IP
+    browser= user_agent.browser.family,
+    browser_version= user_agent.browser.version_string,
+    os= user_agent.os.family,
+    os_version= user_agent.os.version_string,
+    device= user_agent.device.family,
+    is_mobile= user_agent.is_mobile,
+    is_tablet= user_agent.is_tablet,
+    is_pc= user_agent.is_pc
+    data_track = data.dict()
+
+    print(f"DEBUG : {client_ip},{browser},{os},{device},mobile : {is_mobile}, PC : {is_pc}\n Data track : {}")
     return {
         "ip": client_ip,
         "received": data.dict()  # transforme le Pydantic model en dict
