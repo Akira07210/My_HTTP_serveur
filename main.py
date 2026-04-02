04c9047e531765f7aef129fe6e65dcb15a11e135
Akira07210 <martin.fayolle@gmail.com>
2026-04-02 11:48:02 +0000

    Gestion des methodes OPTIONS.

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 # Lib
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
 # Mes fichier
 from log import log_the_visit_in_CSV
 
 app = FastAPI()
-    
+
+# Autorisation des methodes OPTIONS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 ###################################################
 ################## MES ENDPOINTS ##################
 
@@ -14,16 +25,15 @@ class Data(BaseModel):
     page: str
     screen : str
 
-@app.get("/log_your_visit")
+@app.post("/log_your_visit")
 async def log_visit(data: Data, request: Request):
-
     client_ip = request.client.host
     user_agent = request.headers.get("user-agent")
     data_track = data.dict()
 
-    print(f"DEBUG : {client_ip}, Data track : {data_track}")
     return {
-        "ip": client_ip,
+	"IP": client_ip ,
+	"agent": user_agent,
         "received": data.dict()  # transforme le Pydantic model en dict
     }
     
